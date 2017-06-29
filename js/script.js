e20ebf913722f8ee05b567b11a09b78964900257
Mishann <mishashnitser@gmail.com>
2017-06-29 16:33:21 +0300

    Search by city name

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
 $(document).ready(function(){
-    $.ajax({
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=11dcac77a39ddf638dee5d92f346e232',
-      type: 'get',
-      success: function(res){
-        var celsiusTemperature = Math.round(res.main.temp - 273.15) + '&deg;C;';
-        var humidity = res.main.humidity + ' %';
-        var pressure = res.main.pressure + ' hpa';
-        var city = res.name;
-        var country = res.sys.country;
 
-      $('#temperature').html(celsiusTemperature);
-      $('#pressure').text(pressure);
-      $('#humidity').text(humidity);
-      $('#city').text(city);
-      $('#country').text(country);
+getWeather('Lviv');
 
-    },
-  })
+function getWeather(city){
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather',
+    data: {
+        q : city,
+        appid : '11dcac77a39ddf638dee5d92f346e232'
+      },
+    type: 'get',
+    success: function(res){
+      var celsiusTemperature = Math.round(res.main.temp - 273.15) + '&deg;C;';
+      var humidity = res.main.humidity + ' %';
+      var pressure = res.main.pressure + ' hpa';
+      var city = res.name;
+      var country = res.sys.country;
+
+    $('#temperature').html(celsiusTemperature);
+    $('#pressure').text(pressure);
+    $('#humidity').text(humidity);
+    $('#city').text(city);
+    $('#country').text(country);
+
+  },
+  error: function(error){
+  alert('Error');
+  },
+})
+}
+
+$('#submitBtn').click(function(e){
+  e.preventDefault();
+  var city = $('#cityName').val();
+  getWeather(city);
+});
+
+
 })
